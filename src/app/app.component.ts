f5fc9e42939d31ae46fcef77dd8da3f59bea12a6
pasanbasuru <basurupasan@gmail.com>
2019-11-12 18:42:21 +0530

    dist add2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = "black-dashboard-angular";
+
+  ngOnInit() {
+    const OneSignal = window['OneSignal'] || [];
+    console.log("Init OneSignal");
+    OneSignal.push(["init", {
+      appId: "e60daa71-cf77-4baa-aa9b-8b1c4b9942a2",
+      autoRegister: false,
+      allowLocalhostAsSecureOrigin: true,
+      notifyButton: {
+        enable: false
+      }
+    }]);
+    console.log('OneSignal Initialized');
+    OneSignal.push(function () {
+      console.log('Register For Push');
+      OneSignal.push(["registerForPushNotifications"])
+    });
+    OneSignal.push(function () {
+      // Occurs when the user's subscription changes to a new value.
+      OneSignal.on('subscriptionChange', function (isSubscribed) {
+        console.log("The user's subscription state is now:", isSubscribed);
+        OneSignal.getUserId().then(function (userId) {
+          console.log("User ID is", userId);
+        });
+      });
+    });
+    }
 }
